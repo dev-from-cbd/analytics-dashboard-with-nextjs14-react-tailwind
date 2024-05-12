a0519002a2e8c8491f5cb8519c95bb73c367d507
dev-from-cbd <veryfatwombat@gmail.com>
2024-05-13 07:28:16 +1000

    Analytics Dashboard with Next.js 14, React, etc..

---
+++ src/app/analytics/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <p>Modern Analytics Dashboard with Next.js 14, React, Tailwind</p>;
+};
+
+export default Page;
---
+++ src/utils/analytics.ts
@@ -0,0 +1,9 @@
+export class Analytics {
+    private retantion: number = 60 * 60 * 24 * 7
+
+    constructor(opts?: AnalyticsArgs){
+        
+    }
+}
+
+const analytics = new Analytics()

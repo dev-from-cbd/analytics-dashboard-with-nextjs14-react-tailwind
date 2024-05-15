287485ab730bde92a2ae4876886080c3333ca695
dev-from-cbd <veryfatwombat@gmail.com>
2024-05-16 05:56:10 +1000

    Dashboard - Next.js 14, React, Redis, Tailwind

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,5 +1,9 @@
-const Page = () => {
-  return <p>Modern Analytics Dashboard with Next.js 14, React, Tailwind</p>;
+import { analytics } from "@/utils/analytics";
+
+const Page = async () => {
+  const pageview = await analytics.retrieve("pageview", "11/02/2024");
+
+  return <pre className="text-white">{JSON.stringify(pageview)}</pre>;
 };
 
 export default Page;
--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -27,6 +27,25 @@ export class Analytics {
         await redis.hincrby(key, JSON.stringify(event), 1)
         if (!opts?.persist) await redis.expire(key, this.retention)
     }
+
+    async retrieveDays(namespace: string, nDays: number) {
+        type AnalyticsPromise = ReturnType<typeof analytics.retrieve>
+        const promises: AnalyticsPromise[] = []
+        for (let i = 0; i < array.length; i++) {
+            const element = array[i];
+        }
+    }
+
+    async retrieve(namespace: string, date: string) {
+        const res = await redis.hgetall<Record<string, string>>(`analytics::${namespace}::${date}`)
+
+        return {
+            date,
+            events: Object.entries(res ?? []).map(([key, value]) => ({
+                [key]: Number(value)
+            }))
+        }
+    }
 }
 
 export const analytics = new Analytics()
